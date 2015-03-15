f509518116f07b37034fded4fb0d8df91382ef61
Abdul Quddus <abdul_quddus@live.com>
2015-03-15 19:27:10 +0500

    email with pdf

---
+++ protected/views/users/information.php
@@ -0,0 +1,13 @@
+<article class="col1">
+				<h2><span>Data </span>Retrieval</h2>
+				<form id="ContactForm" name="information" action="information" method="POST">
+					<div>
+						
+				                   
+                                                Please enter your secret KEY:<div  class="wrapper"><input name="key" type="text" class="input" /></div>
+						
+                                               <br/>
+						<input name="information" type="submit" value="submit">
+					</div>
+				</form>
+	   		</article>
